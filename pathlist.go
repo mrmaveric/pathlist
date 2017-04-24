530523b911ff09d8f4e27dcacf39274217d46351
James Bull <jpjbull@bcsonline.com.au>
2017-04-24 13:01:06 +1000

    Entry seperator now defined in os specific source
    Since the compiled binary will only work on one OS it seems like a waste
    to check the OS every time the program is run, so I've moved the check
    to compile time.

--- pathlist.go
+++ pathlist.go
@@ -1,21 +1,18 @@
+/*
+Package pathlist is a cli tool for listing your PATH environment variable
+with one entry per line.
+
+pathlist takes no arguements.
+*/
 package main
 
 import (
 	"fmt"
 	"os"
 	"strings"
-	"runtime"
 )
 
 func main() {
-	var sep string;
-
-	if runtime.GOOS == "windows" {
-		sep = ";"
-	} else {
-		sep = ":"
-	}
-
 	for _, entry := range strings.Split(os.Getenv("PATH"), sep) {
 		fmt.Println(entry)
 	}
